a8b17ac07250f997f05b5a2b235ac3f664de5b80
nicholas <nicholasovunda@gmail.com>
2022-06-08 21:37:00 +0100

    basic icons for memory game

--- app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
@@ -1,23 +1,50 @@
 package com.example.memoryapp
 
 import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.core.view.setMargins
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
-class MemoryBoardAdapter(context: Context, private val numPieces: Int) : RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+    RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton =  itemView.findViewById<ImageButton>(R.id.imageButton)
+        fun bind(position: Int) {
+    imageButton.setOnClickListener{
+        Log.i(TAG,"Clicked on position $position")
+    }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardWidth = parent.width /  2 - (2 * MARGIN_SIZE )
+        val cardSideLength = min(cardWidth, cardHeight)
+     val view :View =LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+
+        val layOutParams : ViewGroup.MarginLayoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layOutParams.height = cardSideLength
+        layOutParams.width = cardSideLength
+        layOutParams.setMargins(MARGIN_SIZE)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.bind(position)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = numPieces
+
 }

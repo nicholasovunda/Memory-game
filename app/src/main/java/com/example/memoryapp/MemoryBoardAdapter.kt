f39123cf8e2551f22c1dee5fbd676c1b1688174b
nicholas <nicholasovunda@gmail.com>
2022-06-08 13:10:19 +0100

    first commit

---
+++ app/src/main/java/com/example/memoryapp/MemoryBoardAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.memoryapp
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryBoardAdapter(context: Context, private val numPieces: Int) : RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
